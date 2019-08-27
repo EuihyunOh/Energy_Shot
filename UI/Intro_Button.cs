bcac3a1181090955523b587e453046db830ba124
Aqours <poweroeh@seoultech.ac.kr>
2019-08-27 16:12:59 +0900

    메인 메뉴 추가
    설정창은 아직 없음

---
+++ UI/Intro_Button.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro_Button : MonoBehaviour
+{
+    public void StartClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SettingClick()
+    {
+
+    }
+
+    public void QuitClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+
+#else        
+        Application.Quit();
+#endif
+    }
+}
